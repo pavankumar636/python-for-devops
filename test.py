557507b17fdb8968a0dbf5b4559e008c15e9a4ac
pavankumar636 <pavankumar636@gmail.com>
2022-01-27 13:28:11 +0530

    jj

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
 print  (" hello this is the first commit")
+print ( 124 + 78)
+
+'''
+Simple IF block
+'''
+a = 30
+b = 5
+result = a + b
+print (f" The {a} + the {b} : {result} " ) #spl char for print statement
+print (f" The {a} + the {b} : \n  {result}"  )
+
+if a > b:
+   print ( " a is gre")
+else: 
+    print ( " b is gre")
+
+
+print ( " /c/Users/cjayapav/OneDrive - Capgemini/Desktop/coding \a")
